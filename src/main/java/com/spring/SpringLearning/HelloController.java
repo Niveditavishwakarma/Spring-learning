e45433d813277b4631385d2f7fa8a639e2500bce
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-02-21 15:52:31 +0530

    [Nivedita] Add . added use case 1

--- src/main/java/com/spring/SpringLearning/HelloController.java
+++ src/main/java/com/spring/SpringLearning/HelloController.java
@@ -1,6 +1,7 @@
 package com.spring.SpringLearning;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,12 @@ public class HelloController {
     public String sayHello() {
         return "Hello from BridgeLabz";
     }
+
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
 }
+
 
 
